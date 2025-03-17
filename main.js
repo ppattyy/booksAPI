23153b7a5c1a40065486e2b500ef01ff87af7004
ppattyy <galla355@msu.edu>
2025-03-16 21:21:00 -0400

    final code

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "https://esm.sh/@supabase/supabase-js"
 const supabaseUrl = 'https://nuoqcrrrmhmusyzsokku.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51b3FjcnJybWhtdXN5enNva2t1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNjY1MjgsImV4cCI6MjA1Nzc0MjUyOH0.DvFQAw40sweaujoxxPijK_vB_YR4sqiVYUYmvTuBxQw'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-// Wrap in async function and wait for DOM
 async function loadBooks() {
   let { data: books } = await supabase
     .from('books')
