b614adc59fc563cb72c053e2aa5ff68064b822cb
ppattyy <galla355@msu.edu>
2025-03-16 21:09:17 -0400

    added code

--- main.js
+++ main.js
@@ -3,11 +3,21 @@ const supabaseUrl = 'https://nuoqcrrrmhmusyzsokku.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51b3FjcnJybWhtdXN5enNva2t1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxNjY1MjgsImV4cCI6MjA1Nzc0MjUyOH0.DvFQAw40sweaujoxxPijK_vB_YR4sqiVYUYmvTuBxQw'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-let { data: books, error } = await supabase
-  .from('books')
-  .select('*')
+// Wrap in async function and wait for DOM
+async function loadBooks() {
+  let { data: books } = await supabase
+    .from('books')
+    .select('*')
 
-for (let book of books) {
-    let bookList = document.getElementById('books');
-    bookList.innerHTML += `<li>${book.title}</li>`;
+  for (let book of books) {
+    let bookTable = document.getElementById('books');
+    bookTable.innerHTML += `
+      <tr>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.ISBN}</td>
+      </tr>`;
   }
+}
+
+document.addEventListener('DOMContentLoaded', loadBooks)
